9e782624d179fd4f6e2988619cd50d52e6534f53
Salma Nurfauziah <123766453+slmaaanf@users.noreply.github.com>
2026-07-21 06:21:13 +0700

    Reorder seeder calls in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,9 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            EventSeeder::class,
-            UserSeeder::class,
-            CategorySeeder::class,
+            CategorySeeder::class, 
+            UserSeeder::class,     
+            EventSeeder::class,    
           ]);
     }
 }
