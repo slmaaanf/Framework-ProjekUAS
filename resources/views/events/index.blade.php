6b6b54b9c4f625e73f87003df9af9809b080fd25
Salma Nurfauziah <123766453+slmaaanf@users.noreply.github.com>
2026-07-21 11:11:14 +0700

    Update index.blade.php

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -9,7 +9,7 @@
                         <div class="row align-items-center">
                             <!-- Gambar Event -->
                             <div class="col-md-3">
-                            <img src="{{ asset('storage/' . $event->image) }}" class="img-fluid rounded" alt="{{ $event->title }}">
+                            <img src="{{ Str::startsWith($event->image, 'http') ? $event->image : asset('storage/' . $event->image) }}" alt="{{ $event->title }}" class="...">
                             </div>
 
                             <!-- Detail Event -->
