838e0691af2aec5596633bd468753d36e8e09466
Salma Nurfauziah <123766453+slmaaanf@users.noreply.github.com>
2026-07-21 07:41:50 +0700

    Modify boot method to include Request parameter
    Add Request parameter to boot method and update comments.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,9 +18,9 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(Request $request): void // 2. Tambahkan (Request $request) di sini
+    public function boot(Request $request): void
     {
-        // 3. Paksa Laravel percaya pada proxy Vercel (Mengatasi Loop Redirect)
+        // Paksa Laravel percaya pada proxy Vercel (Mengatasi Loop Redirect)
         $request->setTrustedProxies(
             ['*'], 
             Request::HEADER_X_FORWARDED_FOR | 
